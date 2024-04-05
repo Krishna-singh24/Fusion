161578553b9c8ef65942cabde6364d085d319748
Krishna-singh24 <krishnasingh070721@gmail.com>
2024-04-06 04:20:52 +0530

    Pages designed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,32 @@
 import './App.css'
 import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Error from './pages/Error'
+
 import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
 
 
   return (
     <Router>
+      <Navbar/>
         <Routes>
-           <Route path={'/a'} element={<Home />} />
+           <Route path={'/'} element={<Home />} />
+           <Route path={'/about'} element={<About />} />
+           <Route path={'/contact'} element={<Contact />} />
+           <Route path={'/login'} element={<Login />} />
+           <Route path={'/signup'} element={<Signup />} />
+           <Route path={'*'} element={<Error />} />
+
         </Routes>
+      <Footer/>
     </Router>
+
   )
 }
 
