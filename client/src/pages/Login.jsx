6fd290850ad20718f43829a94cdfe0ab507cf30d
Krishna-singh24 <krishnasingh070721@gmail.com>
2024-04-09 13:26:17 +0530

    Login and signup customized

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,15 +23,12 @@ const Login = () => {
 
         <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
             <div class="mx-auto max-w-lg">
-                <h1 class="text-center text-2xl font-bold text-teal-600 sm:text-3xl">Get started today</h1>
+                <h1 class="text-center text-2xl font-bold text-teal-600 sm:text-3xl">Welcome back !</h1>
 
-                <p class="mx-auto mt-4 max-w-md text-center text-gray-500">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sunt dolores deleniti
-                    inventore quaerat mollitia?
-                </p>
+                
 
                 <form onSubmit={handleSubmit(onSubmit)} action="#" class="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
-                    <p class="text-center text-lg font-medium">Sign in to your account</p>
+                    <p class="text-center text-lg font-medium">Log in to your account</p>
 
                     <div>
                         <label for="email" class="sr-only">Email</label>
@@ -66,7 +63,7 @@ const Login = () => {
                         type="submit"
                         class="block w-full rounded-lg bg-teal-600 px-5 py-3 text-sm font-medium text-white"
                     >
-                        Sign in
+                        Log in
                     </button>
 
                     <p class="text-center text-sm text-gray-500">
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -32,10 +32,7 @@ const Signup = () => {
             <div class="mx-auto max-w-lg">
                 <h1 class="text-center text-2xl font-bold text-teal-600 sm:text-3xl">Get started today</h1>
 
-                <p class="mx-auto mt-4 max-w-md text-center text-gray-500">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sunt dolores deleniti
-                    inventore quaerat mollitia?
-                </p>
+               
 
                 <form onSubmit={handleSubmit(onSubmit)} action="#" class="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
                     <p class="text-center text-lg font-medium">Signup your account</p>
