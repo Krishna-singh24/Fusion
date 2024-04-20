8f64d51674a19f80dccd41c64595668af35305d0
Krishna-singh24 <krishnasingh070721@gmail.com>
2024-04-20 13:13:47 +0530

    Components created

---
+++ testapp/src/components/Avatar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Avatar = () => {
+  return (
+    <div>
+      
+<img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar" />
+<img className="w-10 h-10 rounded" src="/docs/images/people/profile-picture-5.jpg" alt="Default avatar" />
+
+    </div>
+  )
+}
+
+export default Avatar
---
+++ testapp/src/components/Input.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div>
+
+   <input type="text" id="disabled-input" aria-label="disabled input" className="mb-6 bg-white-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value="Disabled input" disabled />
+    <input type="text" id="disabled-input-2" aria-label="disabled input 2" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value="Disabled readonly input" disabled readonly />
+     
+    </div>
+  )
+}
+
+export default Input
