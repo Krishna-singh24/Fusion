b438814d5c51f6bbc633fd9cc3394d4875a4b6b1
Krishna-singh24 <krishnasingh070721@gmail.com>
2024-04-26 11:49:02 +0530

    frontend designed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css'
 import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Error from './pages/Error'
-import Overview from './pages/docs/Overview'
-import Feedback from './pages/Feedback'
+// import About from './pages/About'
+// import Contact from './pages/Contact'
+// import Login from './pages/Login'
+// import Signup from './pages/Signup'
+// import Navbar from './components/Navbar'
+// import Footer from './components/Footer'
+// import Error from './pages/Error'
+// import Overview from './pages/docs/Overview'
+// import Feedback from './pages/Feedback'
 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Documentation from './pages/Documentation'
+// import Documentation from './pages/Documentation'
 
 const App = () => {
 
