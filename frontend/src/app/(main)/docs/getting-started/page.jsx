c0482900a8805b66330e0f4297e69c79767e5d8c
m-triple-m <triplem656@gmail.com>
2024-04-30 08:51:40 +0530

    added block code

--- frontend/src/app/(main)/docs/getting-started/page.jsx
+++ frontend/src/app/(main)/docs/getting-started/page.jsx
@@ -1,4 +1,6 @@
+'use client';
 import React from 'react'
+import { CopyBlock, dracula } from 'react-code-blocks'
 
 const GettingStarted = () => {
   return (
@@ -6,10 +8,16 @@ const GettingStarted = () => {
       <h4 class="text-xl md:text-2xl lg:text-3xl font-mono">Quick Setup</h4>
       <p class="mb-2 text-gray-500 dark:text-gray-400 ">This guide will help you get started with Fusion UI, including how to run, customize, update, and integrate your project!
 
-First, you need to make sure that you have a working Tailwind CSS project installed and that you also have Node and NPM installed on your machine.</p>
-<h3 class="text-sm md:text-2xl lg:text-3xl font-serif">Require via NPM</h3>
-<span >npm i fusion-reactui</span>
+        First, you need to make sure that you have a working Tailwind CSS project installed and that you also have Node and NPM installed on your machine.</p>
+      <h3 class="text-sm md:text-2xl lg:text-3xl font-serif">Require via NPM</h3>
+      <span ></span>
 
+      <CopyBlock
+        text={'npm i fusion-reactui'}
+        language={'bash'}
+        theme={dracula}
+        wrapLines
+      />
 
     </div>
   )
